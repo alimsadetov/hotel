2e501dc70a9dab4b06ff3f7892803a42312b7281
alimsadetov <alimsadetov2019@mail.ru>
2021-05-30 15:23:36 +0300

    colors and type

---
+++ src/main.js
@@ -0,0 +1,16 @@
+//сюда подключаю все
+import '../node_modules/focus-visible/dist/focus-visible';
+
+import './ui/ui.sass';
+
+
+
+// import $ from 'jquery';
+
+// import '../scss/main.scss';
+// import '../index.html';
+
+
+// const a = 5;
+
+// $('body').css('color', 'red');
