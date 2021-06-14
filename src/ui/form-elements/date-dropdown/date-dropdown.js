871774e97a3ab7cc820bc63b6b07b1e0e693926d
alimsadetov <alimsadetov2019@mail.ru>
2021-06-14 19:08:01 +0300

    date-dropdown

---
+++ src/ui/form-elements/date-dropdown/date-dropdown.js
@@ -0,0 +1,51 @@
+$(function(){
+    $('.date-dropdown__form').daterangepicker({
+        "singleDatePicker": true,
+        "autoApply": true,
+        "locale": {
+            "format": "DD.MM.YYYY",
+            "separator": " - ",
+            "applyLabel": "Apply",
+            "cancelLabel": "Cancel",
+            "fromLabel": "From",
+            "toLabel": "To",
+            "customRangeLabel": "Custom",
+            "weekLabel": "W",
+            "daysOfWeek": [
+                "Вс",
+                "Пн",
+                "Вт",
+                "Ср",
+                "Чт",
+                "Пт",
+                "Сб"
+            ],
+            "monthNames": [
+                "Январь",
+                "Февраль",
+                "Март",
+                "Апрель",
+                "Май",
+                "Июнь",
+                "Июль",
+                "Август",
+                "Сентябрь",
+                "Октябрь",
+                "Ноябрь",
+                "Декабрь"
+            ],
+            "firstDay": 1
+        },
+        "drops": "auto"
+    });
+
+
+
+
+
+
+
+
+
+
+});
---
+++ src/ui/form-elements/range-dropdown/range-dropdown.js
@@ -0,0 +1,49 @@
+$(function(){
+    $('.range-dropdown__form').daterangepicker({
+        "autoApply": true,
+        "locale": {
+            "format": "D MMM",
+            "separator": " - ",
+            "applyLabel": "Apply",
+            "cancelLabel": "Cancel",
+            "fromLabel": "From",
+            "toLabel": "To",
+            "customRangeLabel": "Custom",
+            "weekLabel": "W",
+            "daysOfWeek": [
+                "Вс",
+                "Пн",
+                "Вт",
+                "Ср",
+                "Чт",
+                "Пт",
+                "Сб"
+            ],
+            "monthNames": [
+                "Январь",
+                "Февраль",
+                "Март",
+                "Апрель",
+                "Май",
+                "Июнь",
+                "Июль",
+                "Август",
+                "Сентябрь",
+                "Октябрь",
+                "Ноябрь",
+                "Декабрь"
+            ],
+            "firstDay": 1
+        },
+
+    });
+
+
+
+
+
+
+
+
+
+});
